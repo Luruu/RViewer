660431311506a9e253d4f9ce0c0982476e1a9e4c
Luruu <31493347+Luruu@users.noreply.github.com>
2023-03-22 21:24:58 +0100

    Added new widgets, improved code

--- controller.py
+++ controller.py
@@ -8,7 +8,8 @@ import sys
 
 class Controller():
     def __init__(self):
-        source_path = "test/test.mp4"
+        source_path = "test/test_sub.mkv"
+
         view = View(source_path)
         model = Model(back=10,forward=30)
 
@@ -17,24 +18,40 @@ class Controller():
 
         self.m_player = model.player
 
-        self.set_button_connections()
+        self.set_view_connections()
+        
+        
+        self.play_pause()
+        self.w_player.parse_media()
 
-        self.w_player.play()
-
+        print(self.w_player.vlc_player.get_full_title_descriptions())
+        
+        print("[test] n_subtitles:", self.w_player.get_sub_count())
+        print("[est] get_subtitles:", self.w_player.get_sub())
+        
         self.window.show()
         
         sys.exit(self.window.app.exec())
     
-    def set_button_connections(self):
-        ms_back = self.m_player.ms_back
-        self.window.btnBack.clicked.connect(lambda: self.w_player.go_back(ms_back))
-        self.window.tool_bar.addWidget(self.window.btnBack)
 
+    def play_pause(self):
+        if self.w_player.is_playing():
+            self.w_player.pause()
+            self.window.play_pause_action.setIcon(self.window.icon_play)
+        else:
+            self.w_player.play()
+            self.window.play_pause_action.setIcon(self.window.icon_pause)
 
-        ms_forward = self.m_player.ms_forward
-        print(ms_back,ms_forward)
-        self.window.btnForward.clicked.connect(lambda: self.w_player.go_forward(ms_forward))
-        self.window.tool_bar.addWidget(self.window.btnForward)
+    def changeSpeedVideo(self):
+        new_speed = self.window.speed_slider.value() / 5
+        self.window.label_speed.setText(" " + str(new_speed) + "x")
+        self.w_player.set_rate(new_speed)
+
+    def set_view_connections(self):
+        self.window.btnBack.clicked.connect(lambda: self.w_player.go_back(self.m_player.ms_back))
+        self.window.play_pause_action.triggered.connect(self.play_pause)
+        self.window.btnForward.clicked.connect(lambda: self.w_player.go_forward(self.m_player.ms_forward))
+        self.window.speed_slider.valueChanged.connect(self.changeSpeedVideo)
 
 
 if __name__ == '__main__':
--- view.py
+++ view.py
@@ -25,12 +25,15 @@ class PlayerView():
         self.vlc_istance = vlc.Instance()
         self.vlc_player = self.vlc_istance.media_player_new()
         self.media = self.vlc_istance.media_new(video_path)
+        
         self.vlc_player.set_media(self.media)
+
+        '''
         self.video_info = {"duration" : self.vlc_player.get_length(), 
                       "rate" : self.vlc_player.get_rate()}
+        '''
         
     
-
     def get_istance_vlc_player(self):
         return self.vlc_player
         
@@ -49,21 +52,21 @@ class PlayerView():
     def is_playing(self):
         return self.vlc_player.is_playing()
     
+    def parse_media(self):
+        return self.media.parse()
+    
     def go_back(self, ms):
         new_pos = self.get_time() - ms
         print("a",self.get_time())
         if new_pos >=0:
             self.set_time(new_pos)
-        else:
-            self.stop()
-            self.play()
-        
+
     
     def go_forward(self, ms):
         # self.pause()
         new_pos = self.get_time() + ms 
         duration = self.get_duration()
-        print("b",new_pos, duration)
+        print("b", new_pos, duration)
         if new_pos < duration:
             self.set_time(new_pos)
 
@@ -93,34 +96,43 @@ class PlayerView():
     
     def get_state(self):
         return self.vlc_player.get_state()
-   
     
-    '''
-    to-do: 
-
-    video_get_spu_count
-
-    video_set_spu
-
-    video_get_spu_delay
-
-    video_set_spu_delay
-
-    '''
+    def get_sub_count(self):
+        return self.vlc_player.video_get_spu_count()
+    
+    def get_sub(self):
+        return self.vlc_player.video_get_spu()
+    
+    def get_sub_delay(self):
+        return self.vlc_player.video_get_spu_delay()
+    
+    def set_sub_delay(self, delay):
+        return self.vlc_player.video_set_spu_delay(delay)
+   
+    def set_volume(self, volume):
+        return self.vlc_player.audio_set_volume(volume)
 
 
 class WindowView(QMainWindow):
     def __init__(self,player):
+        sys.argv += ['-platform', 'windows:darkmode=1']
         self.app = QApplication(sys.argv)
+        self.app.setStyle('Fusion')
         super().__init__()
         available_geometry = self.screen().availableGeometry()
         self.resize(available_geometry.width() / 4,
                         available_geometry.height() / 3.5)
-        
-        
+    
         self.setWindowTitle("RViewer")
         
+        self.set_widgets()
+
+        self.add_widgets(player)
+        
+
+    def set_widgets(self):
         self.videoframe = QFrame()
+
         # set videoframe color
         self.palette = self.videoframe.palette()
         self.palette.setColor(QPalette.Window, QColor(0, 0, 0))
@@ -130,37 +142,79 @@ class WindowView(QMainWindow):
 
         self.tool_bar = QToolBar()
         self.tool_bar2 = QToolBar()
-        #per far essere stacked le due toolbar
-        
 
+        # toolbar shown on second toolbar
         self.addToolBar(Qt.BottomToolBarArea, self.tool_bar)
         self.addToolBarBreak(Qt.BottomToolBarArea)
         self.addToolBar(Qt.BottomToolBarArea, self.tool_bar2)
 
 
-
         self.btnBack = QPushButton(self)
-        self.btnBack.setText("-10")          #text
-        self.btnBack.setShortcut('Ctrl+D')  #shortcut key
+        self.btnBack.setText("-10")        
+        self.btnBack.setShortcut('Ctrl+D')
         self.btnBack.move(100,100)
+
         
-
-
+        style = self.style()
+        self.icon_play = QIcon.fromTheme("media-playback-start.png",
+                             style.standardIcon(QStyle.SP_MediaPlay))
+        self.icon_pause = QIcon.fromTheme("media-playback-pause.png",
+                               style.standardIcon(QStyle.SP_MediaPause))
+        
 
         self.btnForward = QPushButton(self)
         self.btnForward.setText("+30")          #text
         self.btnForward.setShortcut('Ctrl+F')  #shortcut key
         self.btnForward.move(100,100)
         
+        # self.tool_bar2.addSeparator()
+
+        self.loadbar = QSlider()
+        self.loadbar.setOrientation(Qt.Horizontal)
+        self.loadbar.setMinimum(0)
+        self.loadbar.setMaximum(1)
+        self.loadbar.setSingleStep(1)
 
 
+        self.speed_slider = QSlider()
+        self.speed_slider.setOrientation(Qt.Horizontal)
+        self.speed_slider.setMinimum(1)
+        self.speed_slider.setMaximum(10)
+        available_width = self.screen().availableGeometry().width()
+        self.speed_slider.setFixedWidth(available_width / 10)
+        self.speed_slider.setValue(5) #default playback value of a video (5/5 = 1)
+        self.speed_slider.setTickInterval(1)
+        self.speed_slider.setTickPosition(QSlider.TicksBelow)
+        self.speed_slider.setToolTip("speed video")
         
+        
+        self.label_speed = QLabel(self)
+        self.label_speed.setText(" 1x")
+        self.label_speed.setAlignment(Qt.AlignCenter)
+        
+
+        spacer = QWidget()
+        spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+
+    def add_widgets(self, player):
+        self.tool_bar2.addWidget(self.loadbar)
+        
+        self.tool_bar.addWidget(self.btnBack)
+
+        self.play_pause_action = self.tool_bar.addAction(self.icon_play, "Play/Pause")
+
+        self.tool_bar.addWidget(self.btnForward)
+
+        self.tool_bar.addWidget(self.spacer)
+        
+        self.tool_bar.addWidget(self.speed_slider)
+
+        self.tool_bar.addWidget(self.label_speed)
 
         self.anchorVLCtoWindow(player, self.videoframe.winId())
+    
         
-        
-
-
 
     def anchorVLCtoWindow(self, player, id):
         if sys.platform.startswith('linux'): # for Linux using the X Server
@@ -171,5 +225,3 @@ class WindowView(QMainWindow):
             player.set_nsobject(id)
         else:
             print("ERROR: this software does not work with", sys.platform)
-        
-        
